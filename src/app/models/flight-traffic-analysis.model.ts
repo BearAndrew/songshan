19d40aefb12d8eb1c797e9224a59d4b4b1f09ad3
BearAndrew <a122333andrew@gmail.com>
2025-12-25 23:03:47 +0800

    feat: 更換 api 路徑

--- src/app/models/flight-traffic-analysis.model.ts
+++ src/app/models/flight-traffic-analysis.model.ts
@@ -4,7 +4,9 @@ export type FlightDirection = 'INBOUND' | 'OUTBOUND';
 export interface FlightTrafficAnalysisRequest {
   dateFrom: string;    // e.g. "2025-12-10 10:00:00"
   dateTo: string;      // e.g. "2025-12-11 10:00:00"
-  type: FlightTrafficType;
+  type: FlightTrafficType;  // e.g. ALL, SCHEDULE, COMM, OTHER
   airline: string;     // e.g. "CI"
   direction: FlightDirection; // "INBOUND" | "OUTBOUND"
+  peer: string; // e.g. 3碼IATA code;
+  flightType: string; // e.g. "intl" ...
 }
--- src/environments/environmen.prod.ts
+++ src/environments/environmen.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // 你的 API Base URL（最後**不要**加斜線）
-  apiBaseUrl: 'http://jasonpolaristw.ddns.net:9025/api'
+  apiBaseUrl: 'http://172.16.24.124/backend'
 };
