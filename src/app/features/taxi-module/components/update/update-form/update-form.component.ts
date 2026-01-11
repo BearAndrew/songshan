7d17d89365592a1514fe3638589a6ee2b80c5d5c
BearAndrew <a122333andrew@gmail.com>
2026-01-11 17:54:03 +0800

    feat: Taxi 修改，匯入計程車資料

--- src/app/features/taxi-module/components/update/update-form/update-form.component.ts
+++ src/app/features/taxi-module/components/update/update-form/update-form.component.ts
@@ -143,4 +143,44 @@ export class UpdateFormComponent {
       this.dateTo = formatted || '';
     }
   }
+
+
+    onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+
+    // ✔ 檢查是否為 Excel
+    const isExcel =
+      file.type ===
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
+      file.type === 'application/vnd.ms-excel';
+
+    if (!isExcel) {
+      alert('請上傳 Excel 檔案 (.xls, .xlsx)');
+      input.value = '';
+      return;
+    }
+
+    // ✔ 建立 FormData
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // ✔ 呼叫 API
+    this.apiService.importTaxi(formData).subscribe({
+      next: (res) => {
+        console.log('匯入成功', res);
+        alert('Excel 匯入成功');
+        input.value = ''; // 清空 input，避免同檔案無法再選
+      },
+      error: (err) => {
+        console.error('匯入失敗', err);
+        alert('Excel 匯入失敗');
+      },
+    });
+  }
 }
