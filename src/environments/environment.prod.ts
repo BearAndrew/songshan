9e6137e62527ee7608fdc9d5024971d3d8ef95e1
BearAndrew <a122333andrew@gmail.com>
2026-02-04 20:46:15 +0800

    feat: prod apiBaseUrl 更改

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://172.16.24.124/backend/api'
+  // apiBaseUrl: 'http://172.16.24.124/backend/api'
+  apiBaseUrl: 'https://wt.tsa.gov.tw/acdm/backend/api'
 };
